ee489f3c2f36a1683e6a6bba33b288dc7ddddef5
Samuel Teuber <teuber@kit.edu>
2022-11-04 10:43:18 +0100

    Moved safe

--- src/jvmMain/kotlin/edu.kit.iti.formal.flavium/WorkerQueue.kt
+++ src/jvmMain/kotlin/edu.kit.iti.formal.flavium/WorkerQueue.kt
@@ -121,8 +121,8 @@ class WorkerQueue(entries: List<Task> = listOf()) : Runnable {
             val task: Task? = queue.poll(60, TimeUnit.SECONDS)
             if (task != null) {
                 execute(task)
-                save()
             }
+            save()
         }
     }
 }
