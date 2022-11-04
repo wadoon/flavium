a6e2e10f0bb4e8cb0c5879d5ef41355d7ea1f14b
Alexander Weigl <weigl@kit.edu>
2022-11-04 11:09:30 +0100

    minors, using KeY's random name generator

--- src/jvmMain/kotlin/edu.kit.iti.formal.flavium/WorkerQueue.kt
+++ src/jvmMain/kotlin/edu.kit.iti.formal.flavium/WorkerQueue.kt
@@ -35,9 +35,9 @@ val RESULT_FOLDER: File = File(System.getProperty("RESULT_FOLDER", "results"))
         mkdirs()
     }
 
-fun loadWorkerQueue(pseudonyms : MutableSet<String>): WorkerQueue {
+fun loadWorkerQueue(pseudonyms: MutableSet<String>): WorkerQueue {
     val wq = if (WORK_QUEUE.exists()) {
-        WorkerQueue(Json.decodeFromString(WORK_QUEUE.readText()), NameGenerator(pseudonyms))
+        WorkerQueue(Json.decodeFromString(WORK_QUEUE.readText()), RandomName(pseudonyms))
     } else
         WorkerQueue()
     val t = Thread(wq)
@@ -52,13 +52,16 @@ data class Task(val id: String, val pseudonym: String, val javaCode: String)
 data class Result(val id: String, val pseudonym: String, val stdout: String, val stderr: String, val status: Int)
 
 
-class WorkerQueue(entries: List<Task> = listOf(), nameGeneratorParam : NameGenerator = NameGenerator(mutableSetOf())) : Runnable {
+class WorkerQueue(
+    entries: List<Task> = listOf(),
+    private val nameGenerator: RandomName = RandomName(mutableSetOf())
+) :
+    Runnable {
     private val queue = LinkedBlockingDeque(entries)
-    private val nameGenerator = nameGeneratorParam;
 
     @Synchronized
     fun emit(javaCode: String): Pair<Task, Int> {
-        val t = Task(UUID.randomUUID().toString(), nameGenerator.getName(), javaCode)
+        val t = Task(UUID.randomUUID().toString(), nameGenerator.randomName, javaCode)
         queue.add(t)
         return t to queue.size
     }
@@ -72,7 +75,7 @@ class WorkerQueue(entries: List<Task> = listOf(), nameGeneratorParam : NameGener
         fun execute(task: Task) {
             var stdout = ""
             var stderr = ""
-            var status = 0
+            var status: Int
 
             try {
                 // fill in the java file
@@ -124,8 +127,8 @@ class WorkerQueue(entries: List<Task> = listOf(), nameGeneratorParam : NameGener
             }
             if (task != null) {
                 execute(task)
+                save()
             }
-            save()
         }
     }
 }
