81e1463cd373c5843e996966225e6d8456bc6f1e
Samuel Teuber <teuber@kit.edu>
2022-11-08 16:37:46 +0100

    Do not allow empty workfolder for reset, do not allow ClassLoader, check for Class.forName

--- kuromasu/reset.sh
+++ kuromasu/reset.sh
@@ -2,32 +2,37 @@
 
 shopt -s extglob
 
+if [ -z "$1" ]
+then
+      echo "No workfolder, exiting..."
+      exit "111";
+fi
+
 WORKFOLDER=$1
 path=$(dirname $(readlink -f "${BASH_SOURCE:-$0}"))
 TEMPLATE="$path/template"
-
-rm -rf ${WORKFOLDER}/!(*.java)
-cp -R $TEMPLATE/* $WORKFOLDER/
-
 cd $WORKFOLDER
+
+rm -rf ./!(*.java)
+cp -R $TEMPLATE/* ./
 
 export CLASSPATH=$(pwd)/kuromasu-1.4-all.jar
 export CLASSNAME=MyKuromasuSolver
 
 javac -cp $CLASSPATH:. $CLASSNAME.java \
-    || javac -encoding cp1252 -cp $CLASSPATH:. $CLASSNAME.java 
+    || javac -encoding cp1252 -cp $CLASSPATH:. $CLASSNAME.java
 
-if [ $? -ne 0 ]; then 
+if [ $? -ne 0 ]; then
 	echo "Could not compile solution. See previous error messages."
 	exit "110";
-fi 
+fi
 
-if javap -v $CLASSNAME.class | grep -q "java/lang/Class.forName"; then
+if javap -v *.class | grep -q "java/lang/Class.forName"; then
     echo "Though shall not use Class.forName"
     exit 111
 fi
 
-if $path/classCheck.py $CLASSNAME.class; then
+if $path/classCheck.py "*.class"; then
 	exit 0
 else
     echo "Class check did not succeed. Please avoid dangerous classes/methods like java.io.File or java.net.Socket."
